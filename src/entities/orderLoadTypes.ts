86da667807d1e066bc60cfbc07c39a94322db14d
akon.ick <alongkon.i@iconkaset.com>
2024-02-16 17:55:25 +0700

    fix config

---
+++ src/entities/orderLoadTypes.ts
@@ -0,0 +1,9 @@
+export interface DataForOrderLoad { 
+    productId: string;
+    quantity: number;
+    productName: string;
+    saleUOMTH: string;
+    productImage: string;
+    productFreebiesId?: string;
+    isFreebie?: boolean;
+}
